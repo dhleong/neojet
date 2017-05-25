a8200b2665206bdb070d2b7a7f2ed8ee0b926b68
Daniel Leong <me@dhleong.net>
2017-05-25 12:43:50 -0400

    Add some (very) preliminary put() support for enhanced editor

--- src/main/kotlin/org/neojet/NeojetEnhancedEditorFacade.kt
+++ src/main/kotlin/org/neojet/NeojetEnhancedEditorFacade.kt
@@ -3,21 +3,24 @@ package org.neojet
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.LogicalPosition
-import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.Key
 import io.neovim.java.Neovim
 import io.neovim.java.event.redraw.CursorGotoEvent
 import io.neovim.java.event.redraw.ModeChangeEvent
 import io.neovim.java.event.redraw.ModeInfoSetEvent
+import io.neovim.java.event.redraw.PutEvent
 import io.neovim.java.event.redraw.RedrawSubEvent
 import io.neovim.java.util.ModeInfo
 import io.reactivex.disposables.CompositeDisposable
 import org.neojet.util.buffer
 import org.neojet.util.bufferedRedrawEvents
+import org.neojet.util.inWriteAction
 import org.neojet.util.input
+import org.neojet.util.runUndoTransparently
 import java.awt.Component
 import java.awt.KeyEventDispatcher
 import java.awt.event.KeyEvent
@@ -29,10 +32,10 @@ import javax.swing.JComponent
 
 val NEOJET_ENHANCED_EDITOR = Key<NeojetEnhancedEditorFacade>("org.neojet.enhancedEditor")
 
-class NeojetEnhancedEditorFacade private constructor(val editor: EditorEx) : Disposable {
+class NeojetEnhancedEditorFacade private constructor(val editor: Editor) : Disposable {
     companion object {
         fun install(editor: Editor): NeojetEnhancedEditorFacade {
-            if (!(editor is EditorImpl) || (editor is TextEditor)) {
+            if (!(editor is EditorImpl || editor is TextEditor)) {
                 throw IllegalArgumentException("$editor is not an EditorEx or TextEditor")
             }
 
@@ -70,6 +73,9 @@ class NeojetEnhancedEditorFacade private constructor(val editor: EditorEx) : Dis
     internal lateinit var modes: List<ModeInfo>
     internal var mode: ModeInfo? = null
 
+    var cursorRow: Int = 0
+    var cursorCol: Int = 0
+
     init {
         subs.add(
             nvim.bufferedRedrawEvents()
@@ -95,8 +101,10 @@ class NeojetEnhancedEditorFacade private constructor(val editor: EditorEx) : Dis
 
     @HandlesEvent fun cursorMoved(event: CursorGotoEvent) {
         event.value[0].let {
+            cursorRow = it.row()
+            cursorCol = it.col()
             editor.caretModel.primaryCaret.moveToLogicalPosition(
-                LogicalPosition(it.row(), it.col())
+                LogicalPosition(cursorRow, cursorCol)
             )
         }
     }
@@ -112,6 +120,31 @@ class NeojetEnhancedEditorFacade private constructor(val editor: EditorEx) : Dis
         }
     }
 
+    @HandlesEvent fun put(event: PutEvent) {
+        if (DumbService.getInstance(editor.project!!).isDumb) return
+
+        val start = editor.caretModel.primaryCaret.offset
+        val line = editor.offsetToLogicalPosition(start).line
+        val nextLineStartOffset = editor.logicalPositionToOffset(LogicalPosition(line + 1, 0))
+        val delta = nextLineStartOffset - 1 - start // -1 to get end of current line
+        val end = minOf(
+            editor.document.textLength - 1,
+            start + minOf(event.value.size, delta)
+        )
+
+        if (end < start) {
+            // usually for drawing status line, etc.
+//            System.out.println("Ignore $event at $start (> $end)")
+            return
+        }
+
+        inWriteAction {
+            runUndoTransparently {
+                editor.document.replaceString(start, end, event.bytesToCharSequence())
+            }
+        }
+    }
+
     private fun updateCursor(mode: ModeInfo) {
         val useBlock = (mode.cursorShape == ModeInfo.CursorShape.BLOCK)
         editor.settings.isBlockCursor = useBlock
@@ -122,6 +155,12 @@ class NeojetEnhancedEditorFacade private constructor(val editor: EditorEx) : Dis
     }
 }
 
+
+private fun PutEvent.bytesToCharSequence(): CharSequence {
+    return value.fold(StringBuilder(value.size), { buffer, value ->
+        buffer.append(value.value)
+    })
+}
 
 private fun Component.belongsTo(parentMaybe: JComponent): Boolean {
 
--- src/main/kotlin/org/neojet/util/IntellijUtil.kt
+++ src/main/kotlin/org/neojet/util/IntellijUtil.kt
@@ -1,14 +1,20 @@
 package org.neojet.util
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.util.Computable
+import com.intellij.openapi.util.Ref
 import com.intellij.openapi.vfs.VirtualFile
 import io.neovim.java.Buffer
 import org.neojet.NVIM_BUFFER_KEY
+import java.awt.Component
 import java.awt.Font
 
 /**
@@ -42,4 +48,43 @@ fun getEditorFont(): Font {
     }
 
     return Font(fontFace, Font.PLAIN, fontSize)
+}
+
+/**
+ * @return a lambda that evaluates the given action as
+ *  a WriteAction
+ */
+fun <T> asWriteAction(action: () -> T): () -> T {
+    return {
+        ApplicationManager.getApplication().runWriteAction(Computable {
+            System.out.println("invoking as write action")
+            action()
+        })
+    }
+}
+
+fun runUndoTransparently(action: () -> Unit) {
+    CommandProcessor.getInstance().runUndoTransparentAction(action)
+}
+
+/**
+ * Execute the given block in a write action on the event dispatch
+ *  thread, waiting for the result
+ */
+fun <T> inWriteAction(action: () -> T): T =
+    inWriteAction(ModalityState.defaultModalityState(), action)
+
+fun <T> inWriteAction(component: Component, action: () -> T): T =
+    inWriteAction(ModalityState.stateForComponent(component), action)
+
+fun <T> inWriteAction(modality: ModalityState, action: () -> T): T {
+    val wrapped = asWriteAction(action)
+    val resultRef = Ref.create<T>()
+    val app = ApplicationManager.getApplication()
+
+    app.invokeAndWait({
+        resultRef.set(wrapped())
+    }, modality)
+
+    return resultRef.get()
 }
